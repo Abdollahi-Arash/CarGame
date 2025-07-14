0481e1a4065230c8da28b9cf7747ae85b69d953f
Arash_abdollahi <arashas175@gmail.com>
2025-07-14 15:16:48 +0330

    Boss enemy
    its the same game with a boss enemy. still buggy

--- CarGame.py
+++ CarGame.py
@@ -19,21 +19,29 @@ pygame.init()
 width=1024
 heigth=1040
 screen = pygame.display.set_mode((width, heigth))
+clock = pygame.time.Clock()
+fps = 100
 pygame.display.set_caption("CarGame")
 car=pygame.image.load("Car.png").convert_alpha()
 Road=pygame.image.load("road.png")
 GameOver=pygame.image.load("GameOver.png").convert_alpha()
 Enemy=pygame.image.load(enemy_choose())
 Enemy2=pygame.image.load(enemy_choose())
+BOSS=pygame.image.load("BOSS.png").convert_alpha()
 Enemy_y=0
 Enemy_y2=-300
 car_x=530
 speed=0.2
 Enemy_x=Line_choose()
 Enemy_x2=Line_choose()
+BOSS_x=Line_choose()
+BOSS_y=0
 Lives=5
 crashed1 = False
 crashed2 = False
+cars_passed=0
+boss_active = False
+boss_speed = 8
 #---------------------------------run only in startup
 
 
@@ -63,22 +71,39 @@ while running:
 
     if speed < 0.1:
         speed=0.1
+
+    if boss_active:
+        BOSS_y += boss_speed
+        if BOSS_y > 1040:
+            boss_active = False
+
     #respawn
     if Enemy_y > 1040:
         Enemy=pygame.image.load(enemy_choose())
         Enemy_y = 0
         Enemy_x=Line_choose()
         crashed1=False
-    
+        cars_passed += 1
     #respawn2
     if Enemy_y2 > 1040:
         Enemy2 = pygame.image.load(enemy_choose())
         Enemy_y2 = 0
+        cars_passed += 1
 
         while True:
             Enemy_x2 = Line_choose()
             if Enemy_x2 != Enemy_x:
                 break
+    
+
+    #boss enemy
+    if not boss_active and cars_passed >= 5:
+        boss_active = True
+        cars_passed = 0
+        BOSS_x = Line_choose()
+        BOSS_y = -200
+
+
 
     
     #Keep the car in Line
@@ -104,6 +129,13 @@ while running:
         Lives-=1
 
 
+    #boss collision
+    if boss_active:
+        boss_rect = BOSS.get_rect(topleft=(BOSS_x, BOSS_y)).inflate(-30, -30)
+        if car_rect.colliderect(boss_rect):
+            print("BOSS CRASH")
+            Lives -= 2
+            boss_active = False
 
     #Gameover Logic
     if Lives < 1:
@@ -123,22 +155,24 @@ while running:
     #gas pedal
 
     if keys[pygame.K_w]:
-        speed+=0.002
+        speed+=0.03
     else:
-        speed-=0.001
+        speed-=0.0001
     
     if keys[pygame.K_s]:
-        speed-=0.002
+        speed-=0.04
 
     #Image loaderaa
     screen.blit(Road,(0, 0))
     screen.blit(car, (car_x, (heigth-200)))
     screen.blit(Enemy,(Enemy_x, Enemy_y))
     screen.blit(Enemy2,(Enemy_x2, Enemy_y2))
-    
+    if boss_active:
+        screen.blit(BOSS, (BOSS_x, BOSS_y))
+
 #---------------------------------code goes here
 
 
-
+    clock.tick(fps)
     pygame.display.flip()
 pygame.quit()
